6de0da421e95bd22fa55be905c835b3dc2bcf586
KiloooNL <benweidenhofer@live.com>
2017-06-09 00:17:16 +0930

    Cleaned up upload script

--- upload.php
+++ upload.php
@@ -1,48 +1,33 @@
 <?php
 require_once("config.php");
 
-$targetDir = UPLOAD_DIR;
-$targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
 
-$uploadOk = 1;
+// A list of permitted file extensions
+$allowed = array('csv');
 
-$fileType = pathinfo($targetFile,PATHINFO_EXTENSION);
+// Get PHP ini max file size
 $maxFileSize = getMaximumFileUploadSize();
 
-if(isset($_POST["submit"])) {
+if(isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0){
 
-}
+	$extension = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
 
-// Check if file already exists
-if (file_exists($targetFile)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+	if(!in_array(strtolower($extension), $allowed)){
+		echo '{"status":"error"}';
+		exit;
+	}
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] >= $maxFileSize) {
-    echo "<p>Sorry, your file is too large. Maximum upload size: $maxFileSize.</p>";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($fileType != "csv") {
-    echo "<p>The file you are trying to upload is not a .CSV file.</p>";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-        echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
-        ?>
-        <a href="index.php?fileToConvert=<?php echo $targetDir . basename($_FILES["fileToUpload"]["name"]); ?>" />Convert to Xero</a>
-    </body>
-    </html>
-<?php
-    } else {
-        echo "<p>Sorry, there was an error uploading your file.</p>";
+    if ($_FILES["fileToUpload"]["size"] >= $maxFileSize) {
+        echo "<p>Sorry, your file is too large. Maximum upload size: $maxFileSize.</p>";
+        $uploadOk = 0;
     }
+
+	if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], UPLOAD_DIR.$_FILES['fileToUpload']['name'])){ ?>
+		{status:success"}<a href="index.php?fileToConvert=<?php echo $targetDir . basename($_FILES["fileToUpload"]["name"]); ?>" />Convert to Xero</a>
+	<?php exit;
+	}
 }
+
+echo '{"status":"error"}';
+exit;
 ?>
