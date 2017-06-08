d3cb62c46d4f889d124c6cc749729b362b4db440
KiloooNL <benweidenhofer@live.com>
2017-06-09 00:20:00 +0930

    Cleaned index.php & added style

--- index.php
+++ index.php
@@ -3,26 +3,21 @@
 // Get config
 require_once("config.php");
 
-if(defined($_GET['qb_file'])) {
-    $qbFile = $_GET['qb_file'];
-} else {
-    $qbFile = "";
-}
+// Perform $_GET checks
+$qbFile = checkGet('qbFile');
+$xeroFile = checkGet('xeroFile');
 
-if(defined($_GET['xero_file'])) {
-    $xeroFile = $_GET['xero_file'];
-} else {
-    $xeroFile = "";
-}
+// Init csv
+$csv = new csv();
 
 if(DEBUG_ENABLED) {
-    if($qbFile == "") {
+    if(!isset($qbFile)) {
         echo "No Quickbooks file specified<br>";
     } else {
         echo "Quickbooks file: " . $qbFile . "<br>";
     }
 
-    if($xeroFile == "") {
+    if(!isset($xeroFile)) {
         echo "No Xero file specified<br>";
     } else {
         echo "Xero file: " . $xeroFile . "<br>";
@@ -30,25 +25,72 @@ if(DEBUG_ENABLED) {
 }
 
 //createCSV();
-if($_GET['fileToConvert']) {
-
-    openCSV($file);
-
-    echo '<pre>';
-    print_r($csv);
-    echo '</pre>';
+if(!checkGet('fileToConvert') == "") {
+    $csv->openCSV(checkGet('fileToConvert'));
 } else {
     ?>
-    <!DOCTYPE html>
-    <html>
-    <body>
+<!DOCTYPE html>
+<head>
+<title>Quickbooks2Xero</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select file to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload File" name="submit">
-    </form>
+<script src="js/jquery.min.js"></script>
 
-    </body>
-    </html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+</head>
+<body>
+
+<h1>Quickbooks2Xero</h1>
+<div class="upload">
+		<div class="login-form">
+            <form id="upload" action="upload.php" method="post" enctype="multipart/form-data">
+	            <div class="button">
+				    <h2>Select Quickbooks CSV to upload:</h2><br>
+                        <div class="fileUpload btn btn-primary">
+                            <span>Browse...</span>
+				            <div class="browse"><input type="file" name="fileToUpload" id="fileToUpload" value="browse" class="upload"/></div>
+                        </div>
+				</div>
+
+                        <div class="cancelBtn btn btn-warning">
+                            <span>Cancel</span>
+                            <div class="cancel"><a class="cancelBtn" name="cancel" href="index.php"></a></div>
+                        </div>
+
+                    <div class="submitBtn btn btn-success">
+                        <span>Convert 2 Xero</span>
+				        <div class="done"><input type="submit" name="submit" class="submitBtn"/></div>
+                    </div>
+                    <div class="clear"> </div>
+                </div>
+				<ul>
+				<!-- The file uploads will be shown here -->
+				</ul>
+            </form>
+		</div>
+
+	<!-- JavaScript Includes -->
+		<script src="js/jquery.knob.js"></script>
+	<!-- JavaScript Includes -->
+
+	<!-- jQuery File Upload Dependencies -->
+		<script src="js/jquery.ui.widget.js"></script>
+		<script src="js/jquery.iframe-transport.js"></script>
+		<script src="js/jquery.fileupload.js"></script>
+	<!-- jQuery File Upload Dependencies -->
+
+	<!-- Main JavaScript file -->
+		<script src="js/script.js"></script>
+	<!-- Main JavaScript file -->
+
+
+</div>
+
+</body>
+</html>
     <?php } ?>
