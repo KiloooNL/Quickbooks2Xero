ab8f7f4b2a3a67846784c70ec03a13f771f1f820
Ben Weidenhofer <benweidenhofer@live.com>
2017-06-08 15:38:21 +0930

    Added more config options

--- config.php
+++ config.php
@@ -2,7 +2,12 @@
 define("QUICKBOOKS_CODE", "quickbooks_");
 define("XERO_CODE", "xero_");
 
+// Upload directory
+define("UPLOAD_DIR", "uploads/");
+
 define("DEBUG_ENABLED", true);
+
+$csvInArray = array();
 
 require_once("functions.php");
 
