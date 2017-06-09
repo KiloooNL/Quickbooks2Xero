861002d2cad35fe06d16277971c77622778821fa
Ben Weidenhofer <benweidenhofer@live.com>
2017-06-09 14:32:01 +0930

    Added more error logging

--- upload.php
+++ upload.php
@@ -8,26 +8,33 @@ $allowed = array('csv');
 // Get PHP ini max file size
 $maxFileSize = getMaximumFileUploadSize();
 
-if(isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0){
+if(isset($_POST['submit'])) {
+	if(isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0){
 
-	$extension = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+		$extension = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
 
-	if(!in_array(strtolower($extension), $allowed)){
-		echo '{"status":"error"}';
-		exit;
+		if(!in_array(strtolower($extension), $allowed)){
+			echo '{"status":"error, file ext not allowed"}';
+			exit;
+		}
+
+		if ($_FILES["fileToUpload"]["size"] >= $maxFileSize) {
+			echo "<p>Sorry, your file is too large. Maximum upload size: $maxFileSize.</p>";
+			$uploadOk = 0;
+		}
+
+		if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], UPLOAD_DIR.$_FILES['fileToUpload']['name'])){ ?>
+			{status:success"}<a href="index.php?fileToConvert=<?php echo UPLOAD_DIR . basename($_FILES["fileToUpload"]["name"]); ?>" />Convert to Xero</a>
+		<?php exit;
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	} else {
+	echo '{"status":"error - fileToUpload was not send during POST"}';
 	}
-
-    if ($_FILES["fileToUpload"]["size"] >= $maxFileSize) {
-        echo "<p>Sorry, your file is too large. Maximum upload size: $maxFileSize.</p>";
-        $uploadOk = 0;
-    }
-
-	if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], UPLOAD_DIR.$_FILES['fileToUpload']['name'])){ ?>
-		{status:success"}<a href="index.php?fileToConvert=<?php echo $targetDir . basename($_FILES["fileToUpload"]["name"]); ?>" />Convert to Xero</a>
-	<?php exit;
-	}
+} else {
+	echo '{"status":"error - submit was not send during POST"}';
 }
-
-echo '{"status":"error"}';
+var_dump($_POST,$_FILES);
 exit;
 ?>
