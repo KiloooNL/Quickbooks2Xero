b02e8cdaf8a04ecfdb237eeb67402298237a2655
KiloooNL <benweidenhofer@live.com>
2017-07-11 21:08:02 +0930

    Remove parseCSV function

--- functions.php
+++ functions.php
@@ -3,73 +3,12 @@
 require_once("config.php");
 require_once("classes.php");
 
-
-/**
-function parseCSV($string, $separatorChar = ',', $enclosureChar = '"', $newlineChar = "\n") {
-    // @author: Klemen Nagode
-    $array = array();
-    $size = strlen($string);
-    $columnIndex = 0;
-    $rowIndex = 0;
-    $fieldValue="";
-    $isEnclosured = false;
-    for($i = 0; $i < $size; $i++) {
-
-        $char = $string{$i};
-        $addChar = "";
-
-        if($isEnclosured) {
-            if($char==$enclosureChar) {
-                if($i+1<$size && $string{$i+1}==$enclosureChar){
-                    // escaped char
-                    $addChar=$char;
-                    $i++; // dont check next char
-                } else{
-                    $isEnclosured = false;
-                }
-            } else {
-                $addChar=$char;
-            }
-        } else {
-            if($char==$enclosureChar) {
-                $isEnclosured = true;
-            } else {
-                if($char==$separatorChar) {
-                    $array[$rowIndex][$columnIndex] = $fieldValue;
-                    $fieldValue="";
-                    $columnIndex++;
-                } elseif($char==$newlineChar) {
-                    echo $char;
-                    $array[$rowIndex][$columnIndex] = $fieldValue;
-                    $fieldValue="";
-                    $columnIndex=0;
-                    $rowIndex++;
-                } else {
-                    $addChar=$char;
-                }
-            }
-        }
-
-        if($addChar!=""){
-            $fieldValue.=$addChar;
-
-        }
-    }
-
-    if($fieldValue) { // save last field
-        $array[$rowIndex][$columnIndex] = $fieldValue;
-    }
-
-    return $array;
-}*/
-
 function checkGet($getToGet) {
     if(isset($_GET[$getToGet])) {
         $getToGet = $_GET[$getToGet];
         return $getToGet;
     }
 }
-
 
 function convertSizeToBytes($sSize)
 {
